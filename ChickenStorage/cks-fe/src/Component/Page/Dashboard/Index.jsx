02880029643b1981f6048bd4340934c3f440ca89
gdstorage <gadisanandita@gmail.com>
2024-04-04 15:44:27 +0700

    coba push branch

--- ChickenStorage/cks-fe/src/Component/Page/Dashboard/Index.jsx
+++ ChickenStorage/cks-fe/src/Component/Page/Dashboard/Index.jsx
@@ -1,7 +1,7 @@
 export default function DashboardIndex() {
     return (
         <>
-            <div className="lead">Dashboard</div>
+            <div className="lead">Dashboard gadis</div>
             
 
         </>
